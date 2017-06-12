e150d150b01d35f4e09538bebe999f18b8a592aa
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-06-12 12:03:08 -0400

    Get data from API
    - Added axios npm to request data from API
    - Created componentDidMount that makes the calls from the API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  constructor(props){
+    super()
+    this.state = {
+      games: []
+    }
+  }
+
+  componentDidMount(){
+    axios({
+      url:"https://www.mysportsfeeds.com/api/feed/pull/nfl/2016-2017-regular/scoreboard.json?fordate=20161211",
+      method:'get',
+      auth: {
+        username: 'momo989',
+        password: 'moshiko1988'
+      }
+    })
+      .then(res => {
+        console.log(res)
+        // const posts = res.data.data.children.map(obj => obj.data);
+        // this.setState({ posts });
+      });
+  }r
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Fatih</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
